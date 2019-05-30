fd061e5ad96b19b0b44695b9699662bd7e92a8ba
derrrick oseko <33955073+ronaldngoda@users.noreply.github.com>
2019-05-30 12:26:07 +0300

    Add files via upload

---
+++ screencaster.php
@@ -0,0 +1,118 @@
+
+<?php 
+//CONVERT NUMBERS TO WORDS
+function numberTowords($num)
+{ 
+$ones = array( 
+1 => "one", 
+2 => "two", 
+3 => "three", 
+4 => "four", 
+5 => "five", 
+6 => "six", 
+7 => "seven", 
+8 => "eight", 
+9 => "nine", 
+10 => "ten", 
+11 => "eleven", 
+12 => "twelve", 
+13 => "thirteen", 
+14 => "fourteen", 
+15 => "fifteen", 
+16 => "sixteen", 
+17 => "seventeen", 
+18 => "eighteen", 
+19 => "nineteen" 
+); 
+$tens = array( 
+1 => "ten",
+2 => "twenty", 
+3 => "thirty", 
+4 => "forty", 
+5 => "fifty", 
+6 => "sixty", 
+7 => "seventy", 
+8 => "eighty", 
+9 => "ninety" 
+); 
+ 
+$num = number_format($num,2,".",","); 
+$num_arr = explode(".",$num); 
+$wholenum = $num_arr[0]; 
+$decnum = $num_arr[1]; 
+$whole_arr = array_reverse(explode(",",$wholenum)); 
+krsort($whole_arr); 
+$rettxt = ""; 
+foreach($whole_arr as $key => $i){ 
+if($i < 20){ 
+$rettxt .= $ones[$i]; 
+}elseif($i < 100){ 
+$rettxt .= $tens[substr($i,0,1)]; 
+$rettxt .= " ".$ones[substr($i,1,1)]; 
+}else{ 
+$rettxt .= $ones[substr($i,0,1)]." ".$hundreds[0]; 
+$rettxt .= " ".$tens[substr($i,1,1)]; 
+$rettxt .= " ".$ones[substr($i,2,1)]; 
+} 
+if($key > 0){ 
+$rettxt .= " ".$hundreds[$key]." "; 
+} 
+} 
+if($decnum > 0){ 
+$rettxt .= " and "; 
+if($decnum < 20){ 
+$rettxt .= $ones[$decnum]; 
+}elseif($decnum < 100){ 
+$rettxt .= $tens[substr($decnum,0,1)]; 
+$rettxt .= " ".$ones[substr($decnum,1,1)]; 
+} 
+} 
+return $rettxt; 
+} 
+ 
+extract($_POST);
+if(isset($convert))
+{
+echo "<p align='center' style='color:blue'>".numberTowords("$num")."</p>";
+}
+ 
+?>
+
+<?php 
+//MULTIPLY THE NUMBERS WITH ADITIONAL OPERATOR AS IMPLEMENTOR
+// function to multiply  
+// two numbers x and y 
+function multiply($x, $y) 
+{ 
+/* 0 multiplied with  
+anything gives 0 */
+if($y == 0) 
+    return 0; 
+  
+/* Add x one by one */
+if($y > 0 ) 
+    return ($x + multiply($x,  
+                          $y - 1)); 
+  
+/* the case where  
+y is negative */
+if($y < 0 ) 
+    return -multiply($x, -$y); 
+} 
+  
+// Driver Code 
+echo multiply(1, 3); 
+
+
+
+
+// This code is contributed by mits. 
+?> 
+<br/>
+twenty
+<br/>
+raten is not a number
+<br/>
+20
+<br/>
+Seventeen
